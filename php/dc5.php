bb61c826c15130d8311eb8952bf10b0a679da44d
sergio <sergio@gmail.com>
2022-09-21 20:31:25 +0200

    new file:   dc5.php

---
+++ php/dc5.php
@@ -0,0 +1,24 @@
+<?php
+//Esto es un comentario
+/*Esto es un comentario multilinea */
+   $nombre = "sergio";
+   $r = 3;
+?>
+
+
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="widht=device/width. initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Bienvenido a la calculadora <?=$nombre?></h1>
+        <div>
+            <p>Àrea: <?$sr*M_PI?></p>
+            <p>Longitud: <?2*$r*M_PI?></p>
+        </div>
+    </body>
+</html>
