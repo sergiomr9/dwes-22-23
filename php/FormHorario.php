56dbf8071150a637511e644af92924e47525bac1
sergio <sergio@gmail.com>
2022-09-30 16:30:38 +0200

    probar

--- php/FormHorario.php
+++ php/FormHorario.php
@@ -6,7 +6,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="widht=device/width. initial-scale=1.0">
-        <link rel="stylesheet" src="FormHorario.css">
+        <link rel="stylesheet" href="FormHorario.css">
         <title>Document</title>
     </head>
     <body>
@@ -14,29 +14,31 @@
             <div class="formulario">
                 <form action="Horario.php" method="get">
                     <div class="f">
-                        <input type="text" name="nombre" placeholder=" Tu nombre" id="">
+                        
                         <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre" placeholder=" Tu nombre" id="">
 
-                        <input type="text" name="curso" placeholder=" Tu curso" id="">
                         <label for="curso">Curso</label>
+                        <input type="text" name="curso" placeholder=" Tu curso" id="">
 
-                        <input type="text" name="a1" placeholder=" Tu nombre" id="">
                         <label for="a1">Asignaturas</label>
+                        <input type="text" name="a1" placeholder=" Tu nombre" id="">
 
+                        
                         <input type="text" name="a2" placeholder=" Tu nombre" id="">
-                        <label for="a2">Asignaturas</label>
-
+                        
+                        
                         <input type="text" name="a3" placeholder=" Tu nombre" id="">
-                        <label for="a3">Asignaturas</label>
 
+                        
                         <input type="text" name="a4" placeholder=" Tu nombre" id="">
-                        <label for="a4">Asignaturas</label>
-
+                        
+                        
                         <input type="text" name="a5" placeholder=" Tu nombre" id="">
-                        <label for="a5">Asignaturas</label>
-
+                        
+                        
                         <input type="text" name="a6" placeholder=" Tu nombre" id="">
-                        <label for="a6">Asignaturas</label>
+                        
 
                         <button type="submit" class="">Generar Horario</button>
                     </div>
