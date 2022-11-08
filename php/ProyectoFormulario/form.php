3db1a5df12878e7ce134883fa64de2344e207929
sergio <sergio@gmail.com>
2022-11-08 19:30:47 +0100

    a

---
+++ php/ProyectoFormulario/form.php
@@ -0,0 +1,24 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cuerpo">
+        <form action="" method="post">
+            <h2 class="tit">Nombre <span class="requerido">*</span></h2><br>
+            <input type="text" name="nombre" id="nombre">
+            <input type="text" name="apellidos" id="apellidos"><br>
+
+            <h2 class="tit">Fecha de nacimiento <span class="requerido">*</span></h2><br>
+            <input type="datetime" name="fecha" id="fecha">
+        </form>
+    </div>
+</body>
+</html>
